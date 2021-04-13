d7b534bb82794e44cb5357599d6471c3ebafd274
pauldubois98 <pauldubois98@gmail.com>
2021-04-14 01:26:14 +0200

    Written Percolation Fractals - Def;Density;Dimensionality

--- word_count.py
+++ word_count.py
@@ -1,26 +1,36 @@
-import PyPDF2
+import pdfplumber
 
 filename = 'main.pdf'
-
-pdf_file = open(filename, 'r')
-
-read_pdf = PyPDF2.PdfFileReader(filename)
-number_of_pages = read_pdf.getNumPages()
-
-print(number_of_pages, "pages found")
-
-pattern = ' '
 total_number_of_spaces = 0
 
-for page in range(number_of_pages):
-    read_page = read_pdf.getPage(page)
-    page_content = read_page.extractText()
-    counted_spaces_per_page = len(page_content.split())
-    #print(page_content)
-    print(counted_spaces_per_page, 'words')
-    total_number_of_spaces += counted_spaces_per_page
+with pdfplumber.open(filename) as pdf:
+    for page in range(len(pdf.pages)):
+        page_content = pdf.pages[page].extract_text()
+        splitted = page_content.split()
+        words = [s for s in splitted if len(s)>2]
+        counted_spaces = len(words)
+        #print(page_content)
+        print(counted_spaces, 'words')
+        total_number_of_spaces += counted_spaces
 
-print(total_number_of_spaces, 'words in total')
+    
+print(total_number_of_spaces, 'words in total (not including math equations)')
+
+
+print('\n\n')
+
+with pdfplumber.open(filename) as pdf:
+    for page in range(len(pdf.pages)):
+        page_content = pdf.pages[page].extract_text()
+        splitted = page_content.split()
+        counted_spaces = len(splitted)
+        print(counted_spaces, 'words')
+        total_number_of_spaces += counted_spaces
+
+    
+print(total_number_of_spaces, 'words in total (including math equations)')
+
 
 input()
+
 
